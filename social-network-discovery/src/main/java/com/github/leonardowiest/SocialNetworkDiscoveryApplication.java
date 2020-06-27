f5595f6eda8813932b05bb844e3f9b7e205faf1d
Leonardo Wiest <leonardowiest@hotmail.com>
2020-06-27 13:27:51 -0300

    Doc: Servidor eureka para servir como um gateway

---
+++ social-network-discovery/src/main/java/com/github/leonardowiest/SocialNetworkDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.github.leonardowiest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkDiscoveryApplication.class, args);
+	}
+
+}
---
+++ social-network-discovery/src/main/java/com/github/leonardowiest/autoconfiguration/EurekaServer.java
@@ -0,0 +1,8 @@
+package com.github.leonardowiest.autoconfiguration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+public class EurekaServer {
+
+}
